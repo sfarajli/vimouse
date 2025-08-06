5e7785350a19a3cd97ebbe37b864a5b37d2be038
Suleyman Farajli <suleyman@farajli.net>
2025-08-06 17:23:51 +0400

    style: update function declaration and comments

--- vimouse.c
+++ vimouse.c
@@ -9,68 +9,14 @@
 #define MOVE_AMOUNT 10
 #define FAST_AMOUNT 30
 
-Window root;
-Display *display;
+static void die(char *);
+static void GrabKeyboard(void);
+static int is_modifier(KeySym);
 
-int
-is_modifier(KeySym keysym)
-{
-	/* `XK_Shift_L` is used for changing the speed of the cursor */
-	KeySym modifiers[] = {
-		XK_Shift_R,
-		XK_Control_L,
-		XK_Control_R,
-		XK_Alt_L,
-		XK_Alt_R,
-		XK_Meta_L,
-		XK_Meta_R,
-		XK_Super_L,
-		XK_Super_R,
-		XK_Hyper_L,
-		XK_Hyper_R,
-		XK_Caps_Lock,
-		XK_Num_Lock,
-		XK_Scroll_Lock,
-	};
-	int n = sizeof(modifiers) / sizeof(modifiers[0]);
-	for (int i = 0; i < n; i++)
-		if (keysym == modifiers[i])
-			return 1;
-
-	return 0;
-}
-
-
-static void
-die(char *errmsg)
-{
-	fprintf(stderr, "%s\n", errmsg);
-	exit(1);
-}
-
-static void
-GrabKeyboard(void)
-{
-	display = XOpenDisplay(NULL);
-	if (!display)
-		die("Failed to open Xdisplay");
-
-	struct timespec ts = { .tv_sec = 0, .tv_nsec = 1000000  };
-
-	root = DefaultRootWindow(display);
-	/* try to grab keyboard, we may have to wait for another process to ungrab */
-	for (int i = 0; i < 1000; i++) {
-		if (XGrabKeyboard(display, root, True, GrabModeAsync, GrabModeAsync, CurrentTime) == GrabSuccess)
-			return;
-
-		nanosleep(&ts, NULL);
-	}
-	die("Failed to grab keyboard");
-}
-
+static Window root;
+static Display *display;
 
 int main() {
-
     GrabKeyboard();
 
     printf("Mouse control mode activated. Press 'q' to exit.\n");
@@ -96,11 +42,11 @@ int main() {
 	case XK_k: dy = -speed; break;
 	case XK_j: dy = speed;  break;
 
-	case XK_f: // Left click (tap)
+	case XK_f: /* Left click (tap) */
 		   XTestFakeButtonEvent(display, 1, True, CurrentTime);
 		   XTestFakeButtonEvent(display, 1, False, CurrentTime);
 		   break;
-	case XK_g: // Hold and Release left button down
+	case XK_g: /* Hold and Release left button down */
 		   if (ishover) {
 			   XTestFakeButtonEvent(display, 1, False, CurrentTime);
 			   ishover = 0;
@@ -109,15 +55,15 @@ int main() {
 			   ishover = 1;
 		   }
 		   break;
-	case XK_d: // Right click
+	case XK_d: /* Right click */
 		   XTestFakeButtonEvent(display, 3, True, CurrentTime);
 		   XTestFakeButtonEvent(display, 3, False, CurrentTime);
 		   break;
-	case XK_a: // Scroll up
+	case XK_a: /* Scroll up */
 		   XTestFakeButtonEvent(display, 4, True, CurrentTime);
 		   XTestFakeButtonEvent(display, 4, False, CurrentTime);
 		   break;
-	case XK_s: // Scroll down
+	case XK_s: /* Scroll down */
 		   XTestFakeButtonEvent(display, 5, True, CurrentTime);
 		   XTestFakeButtonEvent(display, 5, False, CurrentTime);
 		   break;
@@ -142,4 +88,60 @@ int main() {
     }
 
     return 0;
+}
+
+static void
+die(char *errmsg)
+{
+	fprintf(stderr, "%s\n", errmsg);
+	exit(1);
+}
+
+static void
+GrabKeyboard(void)
+{
+	display = XOpenDisplay(NULL);
+	if (!display)
+		die("Failed to open Xdisplay");
+
+	struct timespec ts = { .tv_sec = 0, .tv_nsec = 1000000  };
+
+	root = DefaultRootWindow(display);
+	/* Try to grab keyboard, we may have to wait for another process to ungrab */
+	for (int i = 0; i < 1000; i++) {
+		if (XGrabKeyboard(display, root, True, GrabModeAsync, GrabModeAsync, CurrentTime) == GrabSuccess)
+			return;
+
+		nanosleep(&ts, NULL);
+	}
+	die("Failed to grab keyboard");
+}
+
+int
+is_modifier(KeySym keysym)
+{
+	/* `XK_Shift_L` is used for changing the speed of the cursor */
+	KeySym modifiers[] = {
+		XK_Shift_R,
+		XK_Control_L,
+		XK_Control_R,
+		XK_Alt_L,
+		XK_Alt_R,
+		XK_Meta_L,
+		XK_Meta_R,
+		XK_Super_L,
+		XK_Super_R,
+		XK_Hyper_L,
+		XK_Hyper_R,
+		XK_Caps_Lock,
+		XK_Num_Lock,
+		XK_Scroll_Lock,
+	};
+
+	int n = sizeof(modifiers) / sizeof(modifiers[0]);
+	for (int i = 0; i < n; i++)
+		if (keysym == modifiers[i])
+			return 1;
+
+	return 0;
 }
